1fa8d7d32601d97fd053abe2553d6045dc000178
Andrés Leone <forestrf@gmail.com>
2017-07-24 13:58:20 +0200

    Diseñando página principal

---
+++ www/index.php
@@ -0,0 +1,42 @@
+<pre>
+Buscador por tags, nombre, sección y almancén
+
+hueco para poner la clave, y quitarla si está puesta
+
+listado de almacenes, con listado de secciones, con listado de objetos. Filtrar con el cuadro de búsqueda
+</pre>
+
+
+<input type="text"/>
+
+<style>
+.almacen, .seccion, .objeto {
+	float: left;
+	border: 1px solid;
+	margin: 2px;
+	padding: 1px;
+	background-color: rgba(0, 0, 0, 0.1);
+}
+.nombre {
+	display: block;
+}
+.clearer {
+	clear: both;
+}
+</style>
+
+<div class="clearer"/>
+
+<div class="almacen">
+	<div class="nombre">Almacén</div>
+	
+	<div class="seccion">
+	<div class="nombre">Sección</div>
+	
+		<div class="objeto">
+			<div class="nombre">Nombre objeto</div>
+			<div class="imagen">Imagen</div>
+			<div class="cantidad">Cantidad</div>
+		</div>
+	</div>
+</div>
